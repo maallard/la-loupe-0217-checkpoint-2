9f9813ccd7a4a5639e78186b872bc4f3a00c4282
Rabin Julien <julienr114@gmail.com>
2017-04-13 15:57:13 +0200

    initialize stack

---
+++ app/js/routes.js
@@ -0,0 +1,58 @@
+angular.module('app')
+    .config(function($stateProvider, $urlRouterProvider, AccessLevels) {
+        $stateProvider
+            .state('anon', {
+                abstract: true,
+                data: {
+                    access: AccessLevels.anon
+                },
+                views: {
+                    'navbar@': {
+                        templateUrl: 'anon/navbar.html',
+                        controller: 'NavbarController'
+                    }
+                }
+            })
+            .state('anon.login', {
+                url: '/login',
+                views: {
+                    'content@': {
+                        templateUrl: 'anon/login.html',
+                        controller: 'LoginController'
+                    }
+                }
+            })
+            .state('anon.register', {
+                url: '/',
+                views: {
+                    'content@': {
+                        templateUrl: 'anon/register.html',
+                        controller: 'RegisterController'
+                    }
+                }
+            });
+        $stateProvider
+            .state('user', {
+                abstract: true,
+                url: '/chat',
+                views: {
+                    'navbar@': {
+                        templateUrl: 'user/navbar.html',
+                        controller: 'NavbarController'
+                    }
+                },
+                data: {
+                    access: AccessLevels.user
+                }
+            })
+            .state('user.chat', {
+                url: '/',
+                views: {
+                    'content@': {
+                        templateUrl: 'user/chat.html',
+                        controller: 'ChatController'
+                    }
+                }
+            });
+        $urlRouterProvider.otherwise('/');
+    });
