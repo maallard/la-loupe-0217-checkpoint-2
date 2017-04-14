b22fd679f5a0e1e0f42aef8c3662e2eee71f801c
olivier-goy <olivier.goy.37@gmail.com>
2017-04-14 10:42:52 +0200

    function of the vue

--- app/js/controllers/chatController.js
+++ app/js/controllers/chatController.js
@@ -3,4 +3,10 @@ angular.module('app')
         UserService.getOne(CurrentUser.user()._id).then(function(res) {
             $scope.user = res.data;
         });
+        $scope.add = "";
+        $scope.posts = [];
+        $scope.addComment = function(){
+          $scope.posts.push($scope.post);
+          console.log($scope.post);
+        };
     });
