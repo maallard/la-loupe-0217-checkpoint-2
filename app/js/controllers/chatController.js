2113b9fa49f340c262f85cdbbf0429f8b7b33e3b
maallard <mar.allard@gmail.com>
2017-04-14 09:32:25 +0200

    Fixed Login

--- app/js/controllers/chatController.js
+++ app/js/controllers/chatController.js
@@ -1,6 +1,4 @@
 angular.module('app')
-    .controller('ChatController', function($scope, CurrentUser, UserService) {
-        UserService.getOne(CurrentUser.user()._id).then(function(res) {
-            $scope.user = res.data;
-        });
+    .controller('ChatController', function($scope, CurrentUser) {
+        $scope.user = CurrentUser.user();
     });
