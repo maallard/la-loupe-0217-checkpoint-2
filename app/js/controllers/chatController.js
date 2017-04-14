93f35e364fe2437f961d20f5a7b004d1a79e3402
Angelique Clery <angeliqueclery@yahoo.fr>
2017-04-14 11:20:35 +0200

    adding a picture

--- app/js/controllers/chatController.js
+++ app/js/controllers/chatController.js
@@ -2,5 +2,7 @@ angular.module('app')
     .controller('ChatController', function($scope, CurrentUser, UserService) {
         UserService.getOne(CurrentUser.user()._id).then(function(res) {
             $scope.user = res.data;
+            $scope.pseudo = user.pseudo;
+            $scope.date = new Date();
         });
     });
