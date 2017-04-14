db2ab77e2721a1f64bc309bca62e465e30716659
Philippe Pinceloup <ph.pinceloup@gmail.com>
2017-04-14 09:56:51 +0200

    Registering a user is OK

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema({
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
         unique: true
     },
+    username: {
+        type: String,
+        required: true
+    },
     password: {
         type: String,
         required: true
