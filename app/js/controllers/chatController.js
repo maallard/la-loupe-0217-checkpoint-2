6aed03e1a9f9ad38e32e55f03d67587e774a4841
peche56 <c.filleul56@gmail.com>
2017-04-14 09:50:33 +0200

    building front

--- app/js/controllers/chatController.js
+++ app/js/controllers/chatController.js
@@ -2,5 +2,14 @@ angular.module('app')
     .controller('ChatController', function($scope, CurrentUser, UserService) {
         UserService.getOne(CurrentUser.user()._id).then(function(res) {
             $scope.user = res.data;
+
+            $scope.newmessages=[];
+
+            $scope.newMessage='';
+            $scope.addNewMessage= function () {
+              newMessages.push(message);
+              $scope.newMessage='';
+
+            };
         });
     });
