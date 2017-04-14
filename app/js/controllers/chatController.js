d22016b0f8096f7df82bf493357f7fb149402fee
DeRosario <davidveiga.pereira@gmail.com>
2017-04-14 10:31:56 +0200

    Création Register Pseudo + Enregistrement dans BDD User // Renvoie bien sur le user.chat // Debut de Frontlane

--- app/js/controllers/chatController.js
+++ app/js/controllers/chatController.js
@@ -3,4 +3,28 @@ angular.module('app')
         UserService.getOne(CurrentUser.user()._id).then(function(res) {
             $scope.user = res.data;
         });
+
+        $scope.commentaireInput = '';
+        $scope.userInput = '';
+        $scope.chat = [];
+
+        // AJOUTER
+        $scope.ajouterCommentaire = function() {
+
+          var userPost = {
+            commentaire : $scope.commentaireInput,
+            user : $scope.userInput,
+
+          };
+
+            $scope.chat.push(userPost)
+
+            $scope.commentaireInput = '';
+            $scope.userInput = '';
+        };
+
+
+        $scope.verification = function() {
+                    console.log($scope.chat);
+                  };
     });
